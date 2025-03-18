180875e6bce2ef6d33624d2c87e920a9beae127d
ombhinav <abhinavgarg0077@gmail.com>
2025-03-18 11:33:30 +0530

    added auth backend

---
+++ week-6-authentication/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "harkiratloveskiara";
+
+const app = express();
+
+app.use(express.json());
+
+let users = [];
+
+function auth(req,res,next){
+    const token = req.headers.token;
+    const decodedInfo = jwt.verify(token, JWT_SECRET);
+    if(decodedInfo.username){
+        req.username = decodedInfo.username;
+        next();
+    }
+    else{
+        res.status(400).json({
+            message: "Error"
+        })
+    }
+}
+
+app.post('/signup',function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    users.push({
+        username: username,
+        password: password
+    })
+    res.status(200).json({
+        message: "You are signed in"
+    })
+})
+
+app.post('/signin',function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    const user = users.find(u => u.username === username && u.password === password);
+    if(user){
+        const token = jwt.sign({
+            username: username
+        },JWT_SECRET);
+        user.token = token;
+        res.json({
+            message: "Verified",
+            token: token
+        })
+    }
+    else{
+        res.status(400).json({
+            message:"Not verified credentials" 
+        })
+    }
+})
+
+app.get('/me',auth, function(req,res){
+        const user = users.find(u => u.username === req.username);
+        res.json({
+            username: user.username,
+            password: user.password
+        })
+})
+
+app.listen(3000,()=>{
+    console.log("Running on 3000");
+})
+
